18f3519dda14e974145c81bcdf09c46925f0f0b0
carlosberrio <carlosberriomon@yahoo.com>
2022-02-10 12:57:08 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-//Punto 4:Programming is like building a multilingual puzzle
+/*Punto 4:Programming is like building a multilingual puzzle*/
 int main()
 {
   puts("Programming is like building a multilingual puzzle");
