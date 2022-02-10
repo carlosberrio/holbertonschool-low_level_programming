a1b1a27cb355911531e4c634368ebe1f9e57e190
carlosberrio <carlosberriomon@yahoo.com>
2022-02-10 13:01:41 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 /*Punto 4:Programming is like building a multilingual puzzle*/
 int main()
 {
-  puts("Programming is like building a multilingual puzzle");
+  puts("\"Programming is like building a multilingual puzzle");
   return 0;
 }
