d3ba1d305595ecd8023cd7eefb00c4fe5552ad89
carlosberrio <carlosberriomon@yahoo.com>
2022-02-11 14:50:02 -0800

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - imprima los numeros del 1 al 9
+ * Return: siempre 0
+ */
+
+int main(void)
+{
+int num;
+for (num = 0; num <= 9; num++)
+printf("%d", num);
+putchar('\n');
+return (0);
+}
