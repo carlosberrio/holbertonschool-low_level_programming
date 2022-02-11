c0d9612930e2efad24026f5116150e61872e117e
carlosberrio <carlosberriomon@yahoo.com>
2022-02-11 15:11:23 -0800

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - imprima los numeros del 1 al 9
+ * Return: siempre 0
+ */
+
+int main(void)
+{
+int num;
+for (num = 48; num <= 57; num++)
+putchar(num);
+putchar('\n');
+return (0);
+}
