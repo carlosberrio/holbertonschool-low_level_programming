5af940133e7a065e611ffc20f383bd81861482a1
carlosberrio <carlosberriomon@yahoo.com>
2022-02-11 15:28:28 -0800

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - imprima el alfabeto al revés, de la z a la a
+ * Return: siempre 0
+ */
+
+int main(void)
+{
+char alf;
+for (alf = 'z'; alf >= 'a'; alf--)
+putchar('\n');
+return (0);
+}
