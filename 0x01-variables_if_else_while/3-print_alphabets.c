6649d4193406d81e42345545aad3518b284805bf
carlosberrio <carlosberriomon@yahoo.com>
2022-02-11 13:58:53 -0800

    task3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,9 +1,19 @@
-#include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - imprima el alfabeto en minúsculas y luego en mayúsculas
+ * Return: siempre 0
+ */
 
 int main(void)
 {
-        int n;
-        /* your code goes there */
-        return (0);
+char alf;
+for (alf = 'a'; alf <= 'z'; alf++)
+putchar(alf);
+for (alf = 'A'; alf <= 'Z'; alf++)
+putchar(alf);
+putchar('\n');
+return (0);
 }
