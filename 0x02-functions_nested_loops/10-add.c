847f52411181a8ad7848f7ca581e5d7a0111a293
carlosberrio <carlosberriomon@yahoo.com>
2022-02-15 12:07:55 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - función que sume 2 enteros
+ * @a: The first integer to be added.
+ * @b: The second integer to be added.
+ * Return: suma 2 enteros
+ */
+
+int add(int a, int b)
+{
+return (a + b);
+}
