cd8f66a5df4ce498b6468467260de3ac5a8da921
carlosberrio <carlosberriomon@yahoo.com>
2022-02-15 09:04:53 -0800

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * Main - escriba una función que calcule el valor absoluto de un entero
+ * _abs - escriba una función que calcule el valor absoluto de un entero
+ * @ab: variable
  * Return: valor absoluto de un entero
  */
 int _abs(int ab)
 {
 if (ab >= 0)
-return(ab);
+return (ab);
 else
-return(-ab); 
+return (-ab);
 }
