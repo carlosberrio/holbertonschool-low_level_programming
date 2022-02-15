10a5116019afceebd18e43dd81c698d9b2631596
carlosberrio <carlosberriomon@yahoo.com>
2022-02-15 08:03:50 -0800

    print alphabet x 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * main - mprimir alfabeto en minuscula x 10 veces
+ * print_alphabet_x10 - mprimir alfabeto en minuscula x 10 veces
  * Return: siempre 0
 */
 
 void print_alphabet_x10(void)
 {
 int A;
-int B=0;
+int B = 0;
 while (B++ <= 9)
 {
 for (A = 97; A < 123; A++)
