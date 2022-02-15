715e69b5efcf20f600ebb37f74507e933cbbcb10
carlosberrio <carlosberriomon@yahoo.com>
2022-02-15 08:32:39 -0800

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - imprimir función que verifica el carácter en minúscula
+ * _islower - imprimir función que verifica el carácter en minúscula
  * Return: siempre 0
 */
 
