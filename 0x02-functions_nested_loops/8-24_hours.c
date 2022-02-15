acea5175d7d544d28da61568bbb0cc377095e3a1
carlosberrio <carlosberriomon@yahoo.com>
2022-02-15 11:16:02 -0800

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * jack_bauer - función que imprima cada minuto del día
+ * Return: cada minuto del día
+ */
+
+void jack_bauer(void)
+{
+int h;
+int m;
+
+for (h = 0; h < 24; h++)
+{
+for (m = 0; m < 60; m++)
+{
+_putchar((h / 10) + '0');
+_putchar((h % 10) + '0');
+_putchar(':');
+_putchar((m / 10) + '0');
+_putchar((m % 10) + '0');
+_putchar('\n');
+}
+}
+}
