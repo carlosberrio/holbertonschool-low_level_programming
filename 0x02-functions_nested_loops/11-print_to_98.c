9eebf4963804be8f7135f64f31a356b75385b60d
carlosberrio <carlosberriomon@yahoo.com>
2022-02-15 18:26:28 -0800

    print_to_98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * print_to_98 - fn impirma todos los números naturales desde n hasta 98
+ * @n: número de inicio
+ * Return: siempre 0
+ */
+
+void print_to_98(int n)
+{
+if (n <= 98)
+{
+for (n = n; n <= 97 ; n++)
+printf("%d, ", n);
+}
+else
+for (n = n; n > 97; n--)
+printf("%d, ", n);
+printf("98\n");
+}
