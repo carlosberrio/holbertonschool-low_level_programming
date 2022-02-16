452db600f02af9925e02157267b1f097678eaed9
carlosberrio <carlosberriomon@yahoo.com>
2022-02-16 09:52:29 -0800

    0 main

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - escribir número aleatorio positivo negativo o cero
+ * Return: siempre 0
+*/
+
+int main(void)
+{
+        int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 5)
+printf("last digit of %d and %i\n", n, n % 10);
+else if (n == 0)
+printf(Last digit of int and is 0\n");
+else if (n < 6)
+printf(Last digit of int ans is less than 6 and not 0\n");
+
+        return (0);
+}
