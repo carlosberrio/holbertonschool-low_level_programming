8db6e9a7ddb887f277b81dbaf547dce4967e2e75
carlosberrio <carlosberriomon@yahoo.com>
2022-02-16 11:23:00 -0800

    1-main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,16 +9,16 @@ int main(void)
 {
 int i;
 
-printf("don’t delete it! :(\n");
+printf("Infinite loop incoming :(\n");
 
 i = 0;
-
+/*
 while (i < 10)
 {
 putchar(i);
 }
-
-printf("don’t delete it! \\o/\n");
+*/
+printf("Infinite loop avoided! \\o/\n");
 
 return (0);
 }
