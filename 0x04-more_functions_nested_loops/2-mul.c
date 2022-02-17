badb5aa95b97f6c9ae6784b7bf97ea0d04abbe07
carlosberrio <carlosberriomon@yahoo.com>
2022-02-17 10:52:06 -0800

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _mul - multiplicación
+ * @a: variable
+ * @b: vriable
+ * Return: multiplicación
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
+
