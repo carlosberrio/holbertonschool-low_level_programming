079f28ddfd8d5f92df947bf89831ad989d89182e
carlosberrio <carlosberriomon@yahoo.com>
2022-02-17 11:12:21 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mprint_number - imprime números 
+ * @a: variable
+ * Return: números
+ */
+void print_numbers(void)
+{
+int a;
+{
+	for (a = 48; a <=57; a++)
+		_putchar(a);
+}
+_putchar('\n');
+}
