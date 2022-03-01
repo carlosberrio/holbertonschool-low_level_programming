89696d077fc98ebde2088b7eaccc7868c6fdd6bf
carlosberrio <carlosberriomon@yahoo.com>
2022-02-28 20:12:30 -0800

    strpbrk

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strpbrk - función que localice una subcadena
+ * @s: caracter
+ * @accept: caracter
+ * Return: 0
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+int k = 0;
+int n;
+
+while (s[k] != '\0')
+{
+n = 0;
+while (accept[n] != '\0')
+{
+if (s[k] == accept[n])
+{
+return (s + k);
+}
+n++;
+}
+k++;
+}
+return (0);
+}
