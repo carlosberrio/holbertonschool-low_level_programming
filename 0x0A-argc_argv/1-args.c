6f4da7e0bb2b01b673e0d43e6afcec132aa8aed5
carlosberrio <carlosberriomon@yahoo.com>
2022-03-04 11:23:10 -0800

    1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,10 +4,15 @@
  * main - imprime el número de argumentos que le pasan
  * @argc: número de argumentos
  * @argv: array que contiene los argumentos
+ * Return: 0
  */
 
 int main(int argc, __attribute__((unused)) char *argv[])
 {
-printf("%d\n", argc - 1);
+int i;
+for (i = 0; i < argc; i++)
+{
+}
+printf("%d\n", i - 1);
 return (0);
 }
