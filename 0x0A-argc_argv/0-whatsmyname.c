75597557594ceea8e46207884081864264040c7a
carlosberrio <carlosberriomon@yahoo.com>
2022-03-04 09:55:28 -0800

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Imprimir el nombre de un programa
+ * @argc: número de argumentos
+ * @argv: array of arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+argc = 0;
+printf("%s\n", argv[argc]);
+return (0);
+}
