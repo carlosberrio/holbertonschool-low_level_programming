91b4a01a9c8cda41357043f991aa7b0428b32a29
carlosberrio <carlosberriomon@yahoo.com>
2022-03-04 13:42:53 -0800

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - programa que sume números positivos
+ * @argc: entero
+ * @argv: array
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+int num, digit, sum = 0;
+for (num = 1; num < argc; num++)
+{
+for (digit = 0; argv[num][digit]; digit++)
+{
+if (argv[num][digit] < '0' || argv[num][digit] > '9')
+{
+printf("Error\n");
+return (1);
+}
+}
+sum += atoi(argv[num]);
+}
+printf("%d\n", sum);
+return (0);
+}
