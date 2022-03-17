d8cc3915b86993a15de815e959af49e2197a6c88
carlosberrio <carlosberriomon@yahoo.com>
2022-03-17 07:48:29 -0700

    task 1

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+/**
+ * int_index - this is main
+ * @array: this is array
+ * @size: this is size
+ * @cmp: this is cmp
+ * Return: -1
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+if (size <= 0)
+return (-1);
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) != 0)
+return (i);
+}
+return (-1);
+}
