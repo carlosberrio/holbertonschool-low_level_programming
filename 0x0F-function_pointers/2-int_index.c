add0510cac1be6b4c70fd0bd1e4798fd84944a94
carlosberrio <carlosberriomon@yahoo.com>
2022-03-17 08:48:13 -0700

    task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -7,10 +7,11 @@
  * Return: -1
  */
 
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
-if (size <= 0)
+if (size <= 0 || array == NULL || cmp == NULL)
 return (-1);
 for (i = 0; i < size; i++)
 {
