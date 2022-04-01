dd6007e0b390c04b066e8e3d8021c8d0628585c6
Carlos Berrio <4634@holbertonschool.com>
2022-04-01 09:51:48 -0500

    commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,12 @@
 
 size_t print_listint(const listint_t *h)
 {
-size_t sum = 0;
+int count = 0;
 
-while (h != NULL)
+for (; h; h = h->next, count++)
 {
-printf("%i\n", h->n);
-h = h->next;
-sum++;
+printf("%d\n", h->n);
 }
-return (sum);
+
+return (count);
 }
