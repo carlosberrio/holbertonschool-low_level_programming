ceca1eb2ccbc3ffe2b39d1a879689504747f2529
Carlos Berrio <4634@holbertonschool.com>
2022-04-01 12:13:14 -0500

    commit

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,16 +2,16 @@
 
 /**
  * free_listint2 - function that frees a listint_t list.
- *@head: 
+ *@head: head
  *Return: 0
  */
 
 void free_listint2(listint_t **head)
 {
 listint_t *tmp;
-if(head == NULL)
+if (head == NULL)
 return;
-while(*head != NULL)
+while (*head != NULL)
 {
 tmp = *head;
 *head = tmp->next;
