a1dbb9a63fc0fac989f13519bf77b4975541a5ba
Carlos Berrio <4634@holbertonschool.com>
2022-04-01 15:16:11 -0500

    commit

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - function that returns the nth node of a listint_t linked list.
+ * get_nodeint_at_index - function that
+ * returns the nth node of a listint_t linked list.
  * @index: unsigned
  * @head: cabecera
  * Return: adress of n node
