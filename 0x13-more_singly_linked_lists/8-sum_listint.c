305511555bc1ae7546260c336c0813234f6ed9c4
Carlos Berrio <4634@holbertonschool.com>
2022-04-01 15:34:35 -0500

    commit

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * sum_listint - function that returns the sum of all the data (n)
+ * of a listint_t linked list
+ * @head: firs node
+ * Return: sum of  all data
+ */
+int sum_listint(listint_t *head)
+{
+int sum = 0;
+if (!head)
+return (0);
+while (head)
+{
+sum += head->n;
+head = head->next;
+}
+return (sum);
+}
