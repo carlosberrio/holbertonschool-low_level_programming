58003524b57c3bfab4bc619abddf99174c74d676
Carlos Berrio <4634@holbertonschool.com>
2022-04-04 20:52:09 -0500

    commit

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * flip_bits - function that returns the number of bits you
+ * would need to flip to get from one number to another.
+ * @n: entero
+ * @m: entero
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+
+{
+unsigned int counter = 0;
+int i = 0;
+
+for (i = 63; i >= 0; i--)
+{
+if (((n ^ m) >> i) & 1)
+counter++;
+}
+return (counter);
+}
