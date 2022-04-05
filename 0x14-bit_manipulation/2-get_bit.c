d651b06eb16a69d6c04df512eb1e3d515524ec4d
Carlos Berrio <4634@holbertonschool.com>
2022-04-04 21:10:16 -0500

    commit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,15 +1,14 @@
 #include "main.h"
-
 /**
  * get_bit - function that returns the value of a bit at a given index.
  * @n: int
  * @index: int
  * Return : int
  */
-
 int get_bit(unsigned long int n, unsigned int index)
 {
 unsigned int counter = 0;
+
 if (index > 63)
 {
 return (-1);
