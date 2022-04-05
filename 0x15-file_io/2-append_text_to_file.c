a2357be6332d1071785936fa435b6d000b9e1c2c
Carlos Berrio <4634@holbertonschool.com>
2022-04-05 16:58:03 -0500

    commit

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,40 +1,27 @@
 #include "main.h"
-
 /**
- * create_file - function that creates a file
- * @filename: pathname to the file
- * @text_content: content to write in the nre file
- * Return: Return 1 on succes, -1 if fails
+ * append_text_to_file - function that appends text at the end of a file.
+ * @filename: pointer a name of the file
+ * @text_content: pointer to string
+ * Return: 1 or -1
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
-	int filedes, writed, size = 0, error = -1;
-
-	/* Check for a valid filename, different to NULL*/
-	if (!filename)
-		return (-1);
-
-	/* Try to open the file and start the fd*/
-	/* If fd is equal to -1 (error), return -1*/
-	filedes = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (filedes == error)
-		return (-1);
-
-	/* If there is 'text_content', write*/
-	if (text_content)
-	{
-		/* Get the size of 'text_contet'*/
-		for (; *(text_content + size); size++)
-		{};
-
-		/* Write 'size' bytes of 'text_content' in filedes*/
-		/* If 'writed' is equal to -1 (error), return -1*/
-		writed = write(filedes, text_content, size);
-		if (writed == error)
-			return (-1);
-	}
-
-	/* Close iostream*/
-	close(filedes);
-	return (1);
+int fileopen, filewrite, i;
+if (filename == NULL)
+return (-1);
+fileopen = open(filename, O_WRONLY | O_APPEND);
+if (fileopen == -1)
+return (-1);
+if (text_content == NULL)
+{
+close(fileopen);
+return (1);
+}
+for (i = 0; text_content[i] != '\0'; i++);
+filewrite = write(fileopen, text_content, i);
+if (filewrite == -1)
+return (-1);
+close(fileopen);
+return (1);
 }
