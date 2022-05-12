f4b79820d91828eade1a95f09b00be14aee85b48
Carlos Berrio <4634@holbertonschool.com>
2022-05-12 11:56:39 -0500

    task0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,11 +1,12 @@
 #include "lists.h"
 
 /**
- * print_listint - this is main funtion
+ * print_dlistint - this is main funtion
  * @h: pointer
- * Return: size_t
+ * Return: counter
  */
 size_t print_dlistint(const dlistint_t *h)
+
 {
     size_t counter = 0;
 
