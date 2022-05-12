f7b0aae0de8c088791c72fbc911877dd550a6742
Carlos Berrio <4634@holbertonschool.com>
2022-05-12 11:57:54 -0500

    task0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,15 +6,13 @@
  * Return: counter
  */
 size_t print_dlistint(const dlistint_t *h)
-
 {
-    size_t counter = 0;
-
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        counter++;
-        h = h->next;
-    }
-    return (counter);
+size_t counter = 0;
+while (h != NULL)
+{
+printf("%d\n", h->n);
+counter++;
+h = h->next;
+}
+return (counter);
 }
