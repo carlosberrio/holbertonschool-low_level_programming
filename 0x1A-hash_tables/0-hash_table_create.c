d6fedf3a8c771c23b728fa4511bbff4f38c68027
Carlos Berrio <4634@holbertonschool.com>
2022-06-03 14:00:03 -0500

    task0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,7 +2,7 @@
 /**
  * hash_table_create - function that creates a has table
  * @size: insigned long int size
- * Return: hash table
+ * Return: pointer to the hash table
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
