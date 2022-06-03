a9bb0f02db05337d7034a5533a15a111f2fcfdbe
Carlos Berrio <4634@holbertonschool.com>
2022-06-03 14:26:02 -0500

    commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,11 +8,11 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
     hash_table_t *table = NULL;
-    
+
     table = malloc(sizeof(hash_table_t));
     if (table == NULL)
         return (NULL);
-    
+
     table->size = size;
     table->array = malloc(sizeof(hash_node_t) * size);
     if (table->array == NULL)
