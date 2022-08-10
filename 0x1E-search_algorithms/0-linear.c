b15d9bde69dc8c061b7936b9f45493e6a8aad6e5
Carlos Berrio <4634@holbertonschool.com>
2022-08-10 13:57:36 -0500

    task0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -18,8 +18,8 @@ int linear_search(int *array, size_t size, int value)
     for (i = 0; i < 10; i++)
     {
         printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-            if(array[i] == value)
-                return (i);
+        if (array[i] == value)
+            return (i);
     }
     return (-1);
 }
