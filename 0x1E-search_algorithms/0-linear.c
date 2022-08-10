dd29d11e7b1bb385aeabb2ccf484b61f7a9465dc
Carlos Berrio <4634@holbertonschool.com>
2022-08-10 13:57:04 -0500

    task0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,8 +1,11 @@
 #include "search_algos.h"
 
 /**
- * @brief 
- * 
+ * linear_search - linear search algorithm
+ * @array: array to search
+ * @size: size of array
+ * @value: value to search for
+ * Return: index of value or -1 if not found
  */
 
 int linear_search(int *array, size_t size, int value)
